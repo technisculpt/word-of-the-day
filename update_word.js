819f2d33c7e6bd4c0c77f22c74b3eefa75201729
mark <marcolinilagana@gmail.com>
2024-06-20 22:03:24 +1000

    debug

--- update_word.js
+++ update_word.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const words = fs.readFileSync('five_letter_words.txt', 'utf8').split('\n').filter(Boolean);
 const randomIndex = Math.floor(Math.random() * words.length);
 const randomWord = words[randomIndex];
+console.log(randomWord);
 let html = fs.readFileSync('index.html', 'utf8');
 html = html.replace(/<h1 id="random-word">.*<\/h1>/, `<h1 id="random-word">${randomWord}</h1>`);
 fs.writeFileSync('index.html', html);
